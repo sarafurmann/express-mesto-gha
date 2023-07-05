3d60f7202161fcf00b4dfb0a0b07e4a50ea8acf5
Andrey Kustov <andrey.kustov@sbermarket.ru>
2023-07-05 13:20:29 +0300

    module14

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.post(
   '/signin',
   celebrate({
     body: Joi.object().keys({
-      email: Joi.email().required(),
+      email: Joi.string().email().required(),
       password: Joi.string().required(),
     }),
   }),
