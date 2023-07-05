9de96058ab2e85c6ebb46e9d98d5092ac2fb0b5f
Andrey Kustov <andrey.kustov@sbermarket.ru>
2023-07-05 13:19:49 +0300

    module14

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post(
       name: Joi.string().required().min(2).max(30),
       about: Joi.string().required().min(2).max(30),
       avatar: Joi.string().required(),
-      email: Joi.email().required(),
+      email: Joi.string().email().required(),
       password: Joi.string().required(),
     }),
   }),
